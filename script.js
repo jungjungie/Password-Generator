ffd903ace8c99c4c980c22af150751a29c0db439
Esther Min <estherjmin@gmail.com>
2020-06-04 17:24:53 -0400

    Set up prompt/confirms for password criteria

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
-// Assignment Code
-// Selects the HTML element (button) with id "generate" and assigns it to the var generateBtn
+// Assigns button to variable generateBtn
 var generateBtn = document.querySelector("#generate");
+
+var lengthInput = prompt("What is the length of your password?")
+var lowerInput = confirm("Do you want to include lowercase?")
+var upperInput = confirm("Do you want to include uppercase?")
+var numberInput = confirm("Do you want to include numbers?")
+var symbolInput = confirm("Do you want to include symbols?")
+
+
+// Possible password characters
+var lowerStr = 'abcdefghijklmnopqrstuvwxyz';
+var upperStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numberStr = '0123456789';
+var symbolStr = ['@', '%', '+', '"', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
+var combinedArr = [];
+var password = [];
+
+// Example
+// var combinedArr = uppercase.concat(numbers, lowercase, symbols);
+
+// Push characters into master array based on user selections
+      // If select lowercase, push lowerStr into combinedArr
+      // If select uppercase, push upperStr into combinedArr
+      // If select numbers, push numberStr into combinedArr
+      // If select symbols, push symbolStr into combinedArr
+
+function generatePassword() {
+      combinedArr = combinedArr.concat(lowerStr, upperStr);
+
+// Based on length chosen, create for loop that loops # of length times and randomly pulls from master array & pushes into a new "array" for pw
+for (var i=0; i < lengthInput; i++) {
+  password.push(Math.floor(Math.random) * combinedArr.length)
+}
+}
+// Print the pw to the box
+
+
+console.log(password);
+
+ 
+  // If no criteria selected, then return error
+  // If length not specified, then return error
+  // If lowercase only specified, then return only lowercase
+  // if (document.getElementById("lowerCheck").checked) {
+  //   console.log("checked");
+  // } else {
+  //   console.log("not checked");
+  // };
+
+  // If uppercase only specified, then return only lowercase
+  // If numbers only specified, then return only lowercase
+  // If symbols only specified, then return only lowercase
+  // lower + upper
+  // lower + numbers
+  // lower + symbols
+  // ...
+  // return "whatever the pw text is";
+// }
 
 // Write password to the #password input
 // Declares a function writePassword
-function writePassword() {
-  // 
-  var password = generatePassword();
-  // Selects the HTML element (textarea that says 'Your Secure Password') and assigns it to the var passwordText
-  var passwordText = document.querySelector("#password");
-  // Sets the value of passwordText to password
-  passwordText.value = password;
+// function writePassword() {
+//   // returns back password from fuction generatePassword()
+//   var password = generatePassword();
+//   // Selects the HTML element (textarea that says 'Your Secure Password') and assigns it to the var passwordText
+//   var passwordText = document.querySelector("#password");
+//   // Sets the value of passwordText to password
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
 // When the button is clicked, writePassword function will run 
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
 // User clicks button
 // Prompt user to select password criteria
-var lengthInput = prompt("What is the length of your password?")
+
 // prompt("Select criteria for your password:")
 
 // User selects pw criteria:
 // Length must be at least 8 characters and no more than 128 characters
 // User selects lowercase, uppercase, numeric and/or special characters
-
 // After user answers each prompt, the input should be validated and at least one character type should be selected
 
 // After all prompts are answered, pw is generated that matches the selected criteria
-var lowerStr = 'abcdefghijklmnopqrstuvwxyz';
-var upperStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var symbolStr = ['@', '%', '+', '', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
-var randomLower = lowerStr.charAt(Math.floor(Math.random() * 26));
-var randomUpper = upperStr.charAt(Math.floor(Math.random() * 26));
-var randomSymbol = symbolStr[(Math.floor(Math.random() * symbolStr.length))];
-var randomNumber = Math.floor(Math.random() * 10);
-
-var charArr = [randomLower, randomUpper, randomSymbol, randomNumber];
-
-var password = "";
-
-for (var i=1; i <= lengthInput; i++) {
-  password = password + (charArr[Math.floor(Math.random() * 4)]);
-}
-
-console.log(password);
-
-// function check () {
-//   document.getElementById("pwLower").checked
-// }
-// if (document.getElementById("pwLower").checked) {
-//   console.log(lowercaseStr.charAt(Math.floor(Math.random() * 26)));
-// }
-
-// function generatePassword(); 
+// console.log(generatePassword);
 
 // console.log(randomLower);
 // console.log(randomUpper);
