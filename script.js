d480a23b7d3b2b251ec19cb71029e53c400ab2da
Esther Min <estherjmin@gmail.com>
2020-06-04 20:00:14 -0400

    final pw generator based on min criteria

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ var upperInput = confirm("Do you want to include uppercase?")
 var numberInput = confirm("Do you want to include numbers?")
 var symbolInput = confirm("Do you want to include symbols?")
 
-
 // Possible password characters
 var lowerStr = 'abcdefghijklmnopqrstuvwxyz';
 var upperStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -37,11 +36,13 @@ if (symbolInput === true) {
   combinedStr = combinedStr.concat(symbolStr);
 }
 
+// Testing if master array works
 console.log(combinedStr);
 
+// Testing if random selection from master array works
 console.log(combinedStr.charAt(Math.floor(Math.random() * combinedStr.length)));
 
-// Function creates pw based on length input and criteria selection
+// Function definition to display password to user
 function writePassword() {
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -49,73 +50,20 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Create pw based on length input and criteria selection
 function generatePassword() {
-
   for (var i=0; i < lengthInput; i++) {
     password = password + combinedStr.charAt(Math.floor(Math.random() * combinedStr.length));
     // password.push(combinedStr.charAt(Math.floor(Math.random() * combinedStr.length)))
   }
   return password;
-
 }
-// Print the pw to the box
 
-
+// Testing if generated pw matches selected criteria
 console.log(generatePassword());
+
+// Calls the function to display password to user
 writePassword();
  
-  // If no criteria selected, then return error
-  // If length not specified, then return error
-  // If lowercase only specified, then return only lowercase
-  // if (document.getElementById("lowerCheck").checked) {
-  //   console.log("checked");
-  // } else {
-  //   console.log("not checked");
-  // };
-
-  // If uppercase only specified, then return only lowercase
-  // If numbers only specified, then return only lowercase
-  // If symbols only specified, then return only lowercase
-  // lower + upper
-  // lower + numbers
-  // lower + symbols
-  // ...
-  // return "whatever the pw text is";
-// }
-
-// Write password to the #password input
-// Declares a function writePassword
-// function writePassword() {
-//   // returns back password from fuction generatePassword()
-//   var password = generatePassword();
-//   // Selects the HTML element (textarea that says 'Your Secure Password') and assigns it to the var passwordText
-//   var passwordText = document.querySelector("#password");
-//   // Sets the value of passwordText to password
-//   passwordText.value = password;
-
-// }
-
 // Add event listener to generate button
-// When the button is clicked, writePassword function will run 
-// generateBtn.addEventListener("click", writePassword);
-
-// User clicks button
-// Prompt user to select password criteria
-
-// prompt("Select criteria for your password:")
-
-// User selects pw criteria:
-// Length must be at least 8 characters and no more than 128 characters
-// User selects lowercase, uppercase, numeric and/or special characters
-// After user answers each prompt, the input should be validated and at least one character type should be selected
-
-// After all prompts are answered, pw is generated that matches the selected criteria
-
-// console.log(generatePassword);
-
-// console.log(randomLower);
-// console.log(randomUpper);
-// console.log(randomSymbol);
-// console.log(randomNumber);
-
-// Display pw as either an alert or written on pg
+generateBtn.addEventListener("click", writePassword);
